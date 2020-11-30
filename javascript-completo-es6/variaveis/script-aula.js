f7d22c587f2594c134780632fa4372401eed7fae
Tony Carvalho <tonylcbarros@gmail.com>
2020-11-30 14:03:14 -0300

    iniciando estudos da Origamid

---
+++ javascript-completo-es6/variaveis/script-aula.js
@@ -0,0 +1,31 @@
+var nome = "Tony";
+var idade = 29;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var nome = "Tony",
+  cidade = "Recife";
+
+console.log(nome, cidade);
+
+// undefined
+var semdefinir;
+console.log(semdefinir);
+
+// console.log(aindaNaoDefine);
+var comida;
+console.log(comida);
+comida = "Pizza";
+console.log(comida);
+
+// mudando variavel
+var time = "Sport";
+time = "Santa Cruz";
+console.log(time);
---
+++ javascript-completo-es6/variaveis/script.exercicio1.js
@@ -0,0 +1,28 @@
+// EXERCÍCIO
+
+// Declarar uma variável com o seu nome
+var nome = "Tony";
+// Declarar uma variável com a sua idade
+var idade = 29;
+// Declarar uma variável com a sua comida
+var comidaFavorita;
+// favorita e não atribuir valor
+comidaFavorita = "açaí";
+// Atribuir valor a sua comida favorita
+var lugarFavorito;
+// Declarar 5 variáveis diferentes sem valores
+var pessoaQueAma = "Clarissa",
+  oQueMaisGostaDeFazer = "Surfar",
+  paraOndeQuerIr,
+  maiorSonho,
+  quantosGatosQuerTer;
+
+console.log(
+  nome,
+  idade,
+  comidaFavorita,
+  lugarFavorito,
+  pessoaQueAma,
+  oQueMaisGostaDeFazer,
+  paraOndeQuerIr
+);
