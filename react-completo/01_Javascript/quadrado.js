a670f7f4b262537568428554936e7eb7aa36bf09
Tony Carvalho <tonylcbarros@gmail.com>
2021-01-13 10:17:26 -0300

    adicionando review javascript

---
+++ react-completo/01_Javascript/quadrado.js
@@ -0,0 +1,14 @@
+function areaQuadrado(l) {
+  return l * l;
+}
+
+function perimetroQuadrado(l) {
+  return l * 4;
+}
+
+const quadrado = {
+  areaQuadrado,
+  perimetroQuadrado,
+};
+
+export default quadrado;
--- react-completo/01_Javascript/script.js
+++ react-completo/01_Javascript/script.js
@@ -1,4 +1,4 @@
-//          Tudo é objeto
+//                   Tudo é objeto
 
 const menu = {
   seletor: '.principal',
@@ -6,11 +6,11 @@ const menu = {
 
 console.log(menu.seletor.toUpperCase());
 
-//          Arrow Function
+//                   Arrow Function
 
 const upperName = (name) => name.toUpperCase();
 
-//          Desestruturação
+//                   Desestruturação
 
 function handleMouse({ clientX, clientY }) {
   console.log(clientX, clientY);
@@ -29,7 +29,7 @@ console.log(lados);
 console.log(perimetro(10));
 document.addEventListener('click', handleMouse);
 
-//          Rest e Spread
+//                    Rest e Spread
 
 // Rest
 function showList(empresa, ...clientes) {
@@ -40,7 +40,7 @@ function showList(empresa, ...clientes) {
 
 showList('Google', 'Tony', 'André', 'Rafael');
 
-// Spread
+//Spread
 const numeros = [10, 5, 20];
 const maior = Math.max(...numeros);
 console.log(maior);
@@ -56,3 +56,67 @@ const carro = {
 carroAno = { ...carro, ano: 2008 };
 
 console.log(carro);
+
+//                    Modules
+
+import quadrado from './quadrado.js';
+import numeroAleatorio from './numero-aleatorio.js';
+
+console.log(quadrado.perimetroQuadrado(5));
+console.log(numeroAleatorio());
+
+//                    Fetch e Async
+
+fetch('https://...')
+  .then((response) => response.json())
+  .then((json) => {
+    console.log(json);
+  });
+
+// mesma coisa de:
+
+async function fetchProdutos(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+const produtos = fetchProdutos('https://...');
+
+//                    Map e Filter
+
+const precos = [
+  'Crédito',
+  'R$ 200',
+  'R$ 400',
+  'Contas Pagar',
+  'R$ 300',
+  'R$ 400',
+  'Meus dados',
+];
+
+const precosFiltro = precos.filter((preco) => preco.includes('R$'));
+const precosNumeros = precosFiltro.map((preco) =>
+  Number(preco.replace('R$ ', '')),
+);
+
+console.log(precosNumeros);
+
+//                    Expressões
+
+const grupoA = 100; // é uma expressão
+const grupoB = 200; // é uma expressão
+
+if (grupoA > grupoB) {
+  console.log('Grupo A venceu');
+} else {
+  console.log('Grupo B venceu');
+} // não é uma expressão
+
+const vencedor = grupoA > grupoB ? 'Grupo A venceu' : 'Grupo B venceu'; // é uma expressão
+console.log(vencedor);
+
+const active = true;
+const button = active && 'Botão está ativo';
+
+console.log(button);
